aef8aa8720612101d4d0e17c19ed4d84781c4717
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-01 04:37:51 +0200

    Added match_case_calculator.py file to control-flow directory

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,28 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation_type = input("Choose the operation (+, -, *, /): ")
+
+match operation_type:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+        
+    case "/":
+        if num2 == 0:
+            print(f"Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+
+    case _:
+        print("Invalid entry, please try again.")
+
