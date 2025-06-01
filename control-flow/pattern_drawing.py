ad71c5cf7dcb59227b40b6b8c48dbea52f5482fc
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-01 21:48:18 +0200

    File added

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,8 @@
-square_size = int(input("Enter the size of the pattern: "))
+size = int(input("Enter the size of the pattern: "))
+
 row = 0
-while row < square_size:
-    print('*' * square_size)
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
     row += 1
