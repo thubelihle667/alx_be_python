1a89b18f60ef004eaf3c3d76b2184163fc8a8253
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-07 03:54:43 +0200

    arithmetic_operations.py file added

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,21 @@
+def perform_operation():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ")
+
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2    
+        case "multiply":    
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "Error: Division by zero is not allowed."
+            result = num1 / num2
+        case _:
+            return "Error: Invalid operation."
+    return f"The result of {operation}ing {num1} and {num2} is: {result}"
+if __name__ == "__main__":
+    print(perform_operation())
