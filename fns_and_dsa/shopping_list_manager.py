d3ca59a7bce0ef824d049875c7012ee2a550128e
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-07 05:47:13 +0200

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,10 +14,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter an item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
-            item = input("Enter an item to remove: ")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item)
         elif choice == '3':
             print(shopping_list)
