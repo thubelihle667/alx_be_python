c296f4e43639258bf51641c06eba4e668c906145
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-06-18 19:51:00 +0200

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,7 +9,7 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}','{self.year}')"
+        return f"Book('{self.title}','{self.author}', {self.year})"
     
     def __del__(self):
         print(f"Deleting {self.title}")
